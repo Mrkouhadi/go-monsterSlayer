cc97ca735fbaf903dee5fb65367054c2c19f68fa
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-02-02 01:34:12 +0800

    feat: added func to fetch data from input

---
+++ interactions/input.go
@@ -0,0 +1,35 @@
+package interactions
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func GetPlayerChoice(specialAttackIsAvailable bool) string {
+	for { // or for true {}... we keep asking for the player choice until we get wht we want
+		PlayerChoice, _ := getInputData()
+		if PlayerChoice == "1" {
+			return "ATTACK"
+		} else if PlayerChoice == "2" {
+			return "HEAL"
+		} else if PlayerChoice == "3" && specialAttackIsAvailable {
+			return "SPECIAL_ATTACK"
+		}
+		fmt.Println("Could not fetch the player's choice. Please try again !")
+	}
+}
+
+func getInputData() (string, error) {
+	fmt.Print("Your Choice: ") // text printed in the same line where the following will be printed
+	inputData, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	inputData = strings.Replace(inputData, "\n", "", -1)
+
+	return inputData, nil
+}
