16496a678443edc5435209caa6ce3c4f93a91c9e
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-02-02 01:33:51 +0800

    feat: added greeting func

--- interactions/output.go
+++ interactions/output.go
@@ -2,6 +2,19 @@ package interactions
 
 import "fmt"
 
-func PrintGreeting() {
-	fmt.Println("Hello world")
+func ShowGreeting() {
+	fmt.Println("MONSTER SLAYER")
+	fmt.Println("Starting a new game...")
+	fmt.Println("Good Luck !")
+}
+
+func ShowAvailableActions(specialAttackIsAvailable bool) {
+	fmt.Println("Please choose your action: ")
+	fmt.Println("---------------------------")
+	fmt.Println("Action (1) Attack Monster ")
+	fmt.Println("Action (2) Heal Yourself ")
+
+	if specialAttackIsAvailable {
+		fmt.Println("Action (3) Special Attack ")
+	}
 }
