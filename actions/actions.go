82fed56e16e300cb595c0de2e93265bb89e27b47
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-02-03 01:07:45 +0800

    feat: returned the overall current health monster and player

--- actions/actions.go
+++ actions/actions.go
@@ -10,7 +10,7 @@ var randGenerator = rand.New(randSource)
 var currMonsterHealth = MONSTERER_HEALTH
 var currPlayerHealth = PLAYER_HEALTH
 
-func AttackMonster(IsSpecialAttack bool) (int, int) {
+func AttackMonster(IsSpecialAttack bool) {
 	minAttackVal := PLAYER_ATTACK_MIN_DMG
 	maxAttackVal := PLAYER_ATTACK_MAX_DMG
 
@@ -20,11 +20,9 @@ func AttackMonster(IsSpecialAttack bool) (int, int) {
 	}
 	dmgVal := generateRandBetween(minAttackVal, maxAttackVal)
 	currMonsterHealth -= dmgVal
-
-	return currPlayerHealth, currMonsterHealth
 }
 
-func HealPlayer() (int, int) {
+func HealPlayer() {
 
 	healVal := generateRandBetween(PLAYER_HEAL_MIN_VAL, PLAYER_HEAL_MAX_VAL)
 	healthDiff := PLAYER_HEALTH - currPlayerHealth
@@ -34,18 +32,19 @@ func HealPlayer() (int, int) {
 	} else {
 		currPlayerHealth = PLAYER_HEALTH
 	}
-	return currPlayerHealth, currMonsterHealth
 }
 
-func AttackPlayer() (int, int) {
+func AttackPlayer() {
 	minAttackVal := MONSTER_ATTACK_MIN_DMG
 	maxAttackVal := MONSTER_ATTACK_MAX_DMG
 
 	dmgVal := generateRandBetween(minAttackVal, maxAttackVal)
 	currMonsterHealth -= dmgVal
-	return currPlayerHealth, currMonsterHealth
 }
 
+func GetHealthAmounts()(int,int){
+	return currPlayerHealth, currMonsterHealth
+}
 func generateRandBetween(min, max int) int {
 	return randGenerator.Intn(max-min) + min
 }
